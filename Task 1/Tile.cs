501658a88d9d124d02447957b7577b4a94eafb14
AKLegend01 <69838671+AKLegend01@users.noreply.github.com>
2020-09-13 17:03:32 +0200

    -Tile class created - Protected X and Y added - public enum added - constructor added - Obstacle class created

---
+++ Task 1/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_1
+{
+    abstract class Tile
+    {
+        protected int X, Y;
+        public enum TileType{Hero, Enemy, Gold, Weapon};
+
+
+        public Tile()
+        {
+
+        }
+
+    }
+}
